df9f171248c90e44749d099040d9b77828851963
Kevin Knapp <krknapp@gmail.com>
2026-09-25 12:04:20 -0400

    compat: expose cognitive classification contract

---
+++ reference/agentmem_ref/cognitive_classification.py
@@ -0,0 +1,4 @@
+"""Compatibility alias for ``agentmem_ref.contracts.cognitive_classification``."""
+import sys
+from .contracts import cognitive_classification as _real
+sys.modules[__name__] = _real
